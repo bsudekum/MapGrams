c2dd60250eb4885fa16fadf0fb91f855b508bed3
rsudekum <bobby@mapbox.com>
2013-07-21 10:34:10 -0400

    Even more

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(function(){
-		$('.open-panel').click()
+		// $('.open-panel').click()
 
 		var model = new Backbone.Model();
 
@@ -11,6 +11,12 @@ $(function(){
 
 		var fit = function(){
 			map.fitBounds(markers.getBounds());	
+		}
+
+		if(location.hash.slice(1,8) !== 'photo=#' && location.hash.slice(0,7) !== '#access'){
+			console.log(location.hash)
+			var photoId = location.hash.slice(7)
+			console.log(photoId)
 		}
 
 		model.set({
@@ -26,7 +32,8 @@ $(function(){
 			liar:false,
 			userId: null,
 			token: token || '',
-			username: ''
+			username: '',
+			photoId: photoId || ''
 		});
 
 		var sat = L.tileLayer('http://{s}.tiles.mapbox.com/v3/bobbysud.map-l4i2m7nd/{z}/{x}/{y}.png');
@@ -35,6 +42,12 @@ $(function(){
 			layers:[basemap],
 			fadeAnimation:false
 		}).setView([37.7746,-122.4373],16).addControl(L.mapbox.geocoderControl('examples.map-vyofok3q'));
+
+		var markers = new L.MarkerClusterGroup({
+			disableClusteringAtZoom:17,
+			maxClusterRadius:50,
+			animateAddingMarkers: false
+		});
 
 		var imgID = '';
 		var hash = new L.Hash(map);
@@ -67,12 +80,6 @@ $(function(){
 		map.addControl(new trashcan());
 		$('.trashcan').append('<a href=""><img src="css/trashcan.png" width=11px height=11px /></a>')
 
-		var markers = new L.MarkerClusterGroup({
-			disableClusteringAtZoom:17,
-			maxClusterRadius:50,
-			animateAddingMarkers: true
-		});
-
 		var ListView = Backbone.View.extend({
 				el: $('body'),
 				events: {
@@ -95,15 +102,29 @@ $(function(){
 							map.addLayer(basemap);
 							map.removeLayer(sat);
 						}else{
-							
 							map.addLayer(sat);
 							map.removeLayer(basemap);
 						}
 				},
 		
 				render: function(){
+					if(photoId){
+						var aToken = $.cookie('token_cookie') || location.hash.substr(1);
+						$.cookie('token_cookie', aToken, { expires: 300 });
+						
+						console.log(photoId)
+						$.ajax({
+					      	type: 'GET',
+							dataType: 'jsonp',
+							cache: true,
+							url: 'https://api.instagram.com/v1/media/' + photoId + '?' + aToken,
+							success: function (photos) {
+								console.log(photos.data.link)
+								runPhoto(photos, fit)
 
-					if(location.hash.length > 25 || $.cookie('token_cookie')) {
+							}
+						});
+					}else if(location.hash.length > 25 || $.cookie('token_cookie')) {
 						
 						var aToken = $.cookie('token_cookie') || location.hash.substr(1);
 						$.cookie('token_cookie', aToken, { expires: 300 });
@@ -313,16 +334,16 @@ $(function(){
 
 		function runPhotos(photos, fit){
 			$.each(photos.data, function(num){
-  				var link = photos.data[num].link;
-  				var username = photos.data[num].user.username;
-  				var profile = photos.data[num].user.profile_picture;
-  				var imgUrl = photos.data[num].images.low_resolution.url;
-  				var imgThumb = photos.data[num].images.thumbnail.url;
-  				var imgId = photos.data[num].id
-  				var filter = 'filter - ' + photos.data[num].filter;
-  				var video = model.get('video');
-				var includePhoto = model.get('photo');
-  				var date = $.timeago(new Date(parseInt(photos.data[num].created_time) * 1000));
+				var link = photos.data[num].link;
+  				var username = photos.data[num].user.username || '';
+  				var profile = photos.data[num].user.profile_picture || '';
+  				var imgUrl = photos.data[num].images.low_resolution.url || '';
+  				var imgThumb = photos.data[num].images.thumbnail.url || '';
+  				var imgId = photos.data[num].id || '';
+  				var filter = 'filter - ' + photos.data[num].filter || '';
+  				var video = model.get('video') || '';
+				var includePhoto = model.get('photo') || '';
+  				var date = $.timeago(new Date(parseInt(photos.data[num].created_time) * 1000)) || '';
 
   				if(photos.data[num].user.full_name){
   					var name = photos.data[num].user.full_name;
@@ -486,5 +507,181 @@ $(function(){
 	  		});
 
 		}
-});
 
+		function runPhoto(photos, fit){
+			var username = photos.data.user.username;
+			var link = photos.data.link;
+			var profile = photos.data.user.profile_picture;
+			var imgUrl = photos.data.images.low_resolution.url;
+			var imgThumb = photos.data.images.thumbnail.url || '';
+			var imgId = photos.data.id || '';
+			var filter = 'filter - ' + photos.data.filter || '';
+			var video = model.get('video') || '';
+			var includePhoto = model.get('photo') || '';
+			var date = $.timeago(new Date(parseInt(photos.data.created_time) * 1000)) || '';
+
+			if(photos.data.user.full_name){
+				var name = photos.data.user.full_name;
+			}else{
+				var name = '';
+			}
+			
+			if(photos.data.likes.count){
+				var likes = photos.data.likes.count;	
+			}else{
+				var likes = 0;
+			}
+
+			if(photos.data.location){
+				
+				var lat = photos.data.location.latitude;
+				var lng = photos.data.location.longitude;
+				var show = true;
+
+				if(photos.data.location.name){
+					var location = 'at ' + photos.data.location.name;
+				}else{
+					var location = '';
+				}
+
+			}else{
+				var lat = 0;
+				var lng = 0;
+				var show = false;
+			}
+
+			if(photos.data.videos){
+				var videoUrl = photos.data.videos.low_resolution.url;
+			}
+
+			if(photos.data.caption){
+					var caption = photos.data.caption.text
+			}else{
+				var caption = '-';
+			}
+			
+			var imageIcon = L.icon({
+				className: 'imgId-' + imgId,
+			    iconUrl: imgThumb,
+			    iconRetinaUrl: imgThumb,
+			    iconSize: [40, 40],
+			    iconAnchor: [20, 20],
+			});
+
+		    var marker = L.marker(new L.LatLng(lat,lng),{
+		    	icon:imageIcon,
+		    	className:'imgId-' + imgId
+		    });
+
+		    var videoIcon = L.divIcon({
+		    	className: 'video-icon',
+		    	html:'Video',
+		    	iconAnchor: [20,20]
+		    });
+
+		    var markerVideo = L.marker(new L.LatLng(lat,lng),{
+		    	icon:videoIcon
+		    });
+
+		    markerVideo.bindPopup(""+
+		    	'<div class="top-text">'+
+		    		
+		    		'<a href="http://instagram.com/' + username + '" target=_blank>'+
+		    			'<img src="' + profile + '" height="40px" width="40px" class="profile"/>'+
+		    			// '<p>' + name + '</p>' +
+		    			'<p>' + username + '</p>' +
+		    		'</a>' +
+
+  		    	'<div class="pull-right">' +
+  		    		'<p>' + date + '</p>' +
+  		    		'<p>' + location + '</p>' +
+  		    		'<p>' + likes + '</p>' +
+  		    	'</div>'+
+		    	'</div>' +
+
+		    	'<a href="' + link + '" target=_blank>'+
+		    		'<video width="273" height="273" autoplay loop><source src="' + videoUrl + '" type="video/mp4">Your browser does not support the video tag.</video>'+
+		    	'</a>'+
+
+		    	'<div class="bottom-text">' +
+		    		'<p>'+caption+'</p>' +
+		    	'</div>',{
+		    	maxWidth:273,
+		    	autoPanPadding: new L.Point(5, 44),
+		    	autoPan:false
+		    });
+
+		    marker.bindPopup(""+
+		    	'<div class="top-text">'+
+		    		
+		    		'<a href="http://instagram.com/' + username + '" target=_blank>'+
+		    			'<img src="' + profile + '" height="40px" width="40px" class="profile"/>'+
+		    			// '<p>' + name + '</p>' +
+		    			'<p>' + username + '</p>' +
+		    		'</a>' +
+
+  		    	'<div class="pull-right">' +
+  		    		'<p>' + date + '</p>' +
+  		    		'<p>' + location + '</p>' +
+  		    		'<p>' + likes + '<span class="heart"> ♥</span></p>' +
+  		    	'</div>'+
+		    	'</div>' +
+
+		    	'<a href="' + link + '" target=_blank>'+
+		    		'<img src="' + imgUrl + '" height="273px" width="273px"/>'+
+		    	'</a>'+
+		    	'<div class="bottom-text">' +
+		    		'<p class="caption">'+caption+'</p>' +
+		    	'</div>',{
+		    	maxWidth:273,
+		    	minWidth: imgId,
+		    	autoPanPadding: new L.Point(5, 44),
+		    	autoPan: false
+		    });
+					
+		if(includePhoto){
+			markers.addLayer(marker);	
+		}
+
+        if(photos.data.videos){
+        	markers.addLayer(markerVideo);
+        }
+        
+  		map.addLayer(markers);
+
+  		marker.on('mouseover', function(e) {
+  			marker.openPopup();
+  		});
+
+  		markerVideo.on('mouseover', function(e) {
+  			markerVideo.openPopup();
+  		});
+
+  		marker.on('click', function(e) {
+			map.panTo(marker.getLatLng());
+		});
+		markerVideo.on('click', function(e) {
+			map.panTo(marker.getLatLng());
+		});
+
+		if(fit){
+			map.fitBounds(markers.getBounds());
+		}
+
+		map.zoomOut();
+		setTimeout(function(){
+			marker.openPopup();	
+		},1500)
+
+  		$('.loading').remove();
+
+		map.on('popupopen', function(e) {
+			var imgID = e.popup.options.minWidth;
+  			window.location.hash ='#photo=' + imgID
+  		}).on('moveend', function(e) {
+			var imgID = '';
+			// window.location.hash = imgID
+  		});
+
+	}
+});
